1e8997bce1e3342f3d50561a83632f1d182fc514
antyee87 <Antyee87@gmail.com>
2026-05-14 20:01:43 +0800

    Architecture type change Transfer nccu_unit to department

--- backend/dataset/architecture.sql
+++ backend/dataset/architecture.sql
@@ -1,13 +1,19 @@
 -- MySQL database architecture for a student course management system (FULL)
 
+CREATE DATABASE IF NOT EXISTS ds_final_project
+CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
+
+USE ds_final_project;
+
 CREATE TABLE IF NOT EXISTS `department` (
-  `department_id` INT(3) NOT NULL,
-  `department_name` VARCHAR(20) NOT NULL,
+  `department_id` VARCHAR(3) NOT NULL,
+  `department_name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`department_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 CREATE TABLE IF NOT EXISTS `graduation_requirements` (
-    `department_id` INT(3) NOT NULL,
+    `department_id` VARCHAR(3) NOT NULL,
     `admit_year` INT NOT NULL,
     `required_major_credits` INT NOT NULL,
     `required_optional_credits` INT NOT NULL,
@@ -17,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `graduation_requirements` (
 );
 
 CREATE TABLE IF NOT EXISTS `student_account` (
-  `student_id` INT(9) NOT NULL,
+  `student_id` VARCHAR(9) NOT NULL,
   `password` VARCHAR(20) NOT NULL,
   `username` VARCHAR(20) NOT NULL,
   `department_major1` INT(3) NOT NULL,
@@ -27,16 +33,16 @@ CREATE TABLE IF NOT EXISTS `student_account` (
   CONSTRAINT `fk_student_account_department_major1` FOREIGN KEY (`department_major1`) REFERENCES `department`(`department_id`),
   CONSTRAINT `fk_student_account_department_major2` FOREIGN KEY (`department_major2`) REFERENCES `department`(`department_id`),
   CONSTRAINT `fk_student_account_department_auxiliary` FOREIGN KEY (`department_auxiliary`) REFERENCES `department`(`department_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 CREATE TABLE IF NOT EXISTS `teacher_account` (
   `teacher_id` VARCHAR(20) NOT NULL,
   `password` VARCHAR(20) NOT NULL,
   `username` VARCHAR(20) NOT NULL,
-  `department_id` INT(3) NOT NULL,
+  `department_id` VARCHAR(3) NOT NULL,
   PRIMARY KEY (`teacher_id`),
   CONSTRAINT `fk_teacher_account_department_id` FOREIGN KEY (`department_id`) REFERENCES `department`(`department_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 CREATE TABLE IF NOT EXISTS `course_information` (
   `course_id` INT(9) NOT NULL,
@@ -46,10 +52,10 @@ CREATE TABLE IF NOT EXISTS `course_information` (
   `department_id` INT(3) NOT NULL,
   `credits` INT(3) NOT NULL,
   PRIMARY KEY (`course_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 CREATE TABLE IF NOT EXISTS `course_record` (
-  `student_id` INT(9) NOT NULL,
+  `student_id` VARCHAR(9) NOT NULL,
   `course_id` INT(9) NOT NULL,
   `semester` VARCHAR(5) NOT NULL,
   `grade` INT(3) NOT NULL,
@@ -57,4 +63,4 @@ CREATE TABLE IF NOT EXISTS `course_record` (
   PRIMARY KEY (`student_id`, `course_id`),
   CONSTRAINT `fk_course_record_student_id` FOREIGN KEY (`student_id`) REFERENCES `student_account`(`student_id`),
   CONSTRAINT `fk_course_record_course_id` FOREIGN KEY (`course_id`) REFERENCES `course_information`(`course_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
